669d644405ad854b4e15e9da5fe35696792f7e7b
Tingye Xie <xiety004@mymail.unisa.edu.au>
2025-10-31 21:00:05 +1030

    add volumn slider, setcpm and toggle soundtracks

--- src/components/Dj_Controls.jsx
+++ src/components/Dj_Controls.jsx
@@ -2,20 +2,31 @@
 function DjControls() {
     return (
         <>
+            <div className="input-group mb-3">
+                <span className="input-group-text" id="cpm_label">setCPM</span>
+                <input type="text" className="form-control" id="cpm_text_input" placeholder="120" aria-label="cpm" aria-describedby="cpm_label"/>
+            </div>
+
+            <label htmlFor="volume_range" className="form-label">Volume</label>
+            <input type="range" className="form-range" min="0" max="1" step="0.01"id="volume_range"/>
+
             <div className="form-check">
-                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" defaultChecked />
-                {/*onChange={ProcAndPlay}*/}
-                <label className="form-check-label" htmlFor="flexRadioDefault1">
-                    p1: ON
+                <input className="form-check-input" type="checkbox" value="" id="s1"/>
+                <label className="form-check-label" htmlFor="s1">
+                    s1
                 </label>
             </div>
+
             <div className="form-check">
-                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
-                {/*onChange={ProcAndPlay}*/}
-                <label className="form-check-label" htmlFor="flexRadioDefault2">
-                    p1: HUSH
-                </label>
+                <input  className="form-check-input" type="checkbox" value="" id="d1" />
+                <label className="form-check-label" htmlFor="d1">d1</label>
             </div>
+
+            <div className="form-check">
+                <input className="form-check-input" type="checkbox" value="" id="d2"/>
+                <label className="form-check-label" htmlFor="d2">d2</label>
+            </div>
+
         </>
     
   );
